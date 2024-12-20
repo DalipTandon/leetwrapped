d1632679bbdf83aa3a380a13116379e58c3d423b
Dalip Tandon <daliptandon8@gmail.com>
2024-12-20 13:38:23 +0530

    profile background done

--- src/app/profile/Content.tsx
+++ src/app/profile/Content.tsx
@@ -4,11 +4,11 @@ const Content=()=>{
     return(
         <div className="flex flex-wrap  h-72 w-full mt-5  justify-between">
             <div className="bg-[#251B13] h-20 w-[49%]    rounded-lg p-3 text-black font-bold bg-gradient-to-r from-amber-600 to-purple-200"> Universal Rank <br/> <p> 32113423</p></div>
-            <div className="bg-[#221921] h-20 w-[49%] rounded-lg bg-gradient-to-r from-purple-500 to-pink-600">dfddfds</div>
-            <div className="bg-[#251B43] h-20 w-[49%]  rounded-lg bg-gradient-to-r from-emerald-300 to-green-600">dfddfds</div>
-            <div className="bg-[#251B83] h-20 w-[49%] rounded-lg  bg-gradient-to-r from-amber-500 to-orange-600">dfddfds</div>
-            <div className="bg-[#251B43] h-20 w-[49%] rounded-lg bg-gradient-to-r from-blue-500 to-cyan-600">dfddfds</div>
-            <div className="bg-[#251B13] h-20 w-[49%] rounded-lg bg-gradient-to-r from-amber-600 to-purple-200">dfddfds</div>
+            <div className="bg-[#221921] h-20 w-[49%] rounded-lg bg-gradient-to-r from-purple-500 to-pink-600 p-3 font-bold">Contest rating <br/> <p>2100</p></div>
+            <div className="bg-[#251B43] h-20 w-[49%]  rounded-lg bg-gradient-to-r from-emerald-300 to-green-600 p-3 font-bold">total question solved <br/> <p>200/300</p></div>
+            <div className="bg-[#251B83] h-20 w-[49%] rounded-lg  bg-gradient-to-r from-amber-500 to-orange-600 p-3 font-bold">easy  <br/> <p>70</p></div>
+            <div className="bg-[#251B43] h-20 w-[49%] rounded-lg bg-gradient-to-r from-blue-500 to-cyan-600 p-3 font-bold">medium <br/> <p>300</p></div>
+            <div className="bg-[#251B13] h-20 w-[49%] rounded-lg bg-gradient-to-r from-amber-600 to-purple-200 p-3 font-bold">hard <br/> <p>100</p></div>
         </div>
     )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,12 @@
 import Chart from "./Chart";
 import Content  from "./Content";
+import React from "react";
+import { BackgroundLines } from "../components/ui/background-lines";
 
-
-const UserProfile=()=>{
-    return(
-        <div className="h-screen bg-[#0A0A0A] flex flex-col items-center justify-center p-8">
-            <div className="bg-[#111111] h-5/6 w-3/6 rounded-lg p-6">
+export function UserProfile() {
+  return (
+    <BackgroundLines className="flex items-center w-full flex-col  bg-black">
+     <div className="bg-[#111111] h-5/6 w-3/6 mt-5 rounded-lg p-6 absolute">
             <h5 className="text-white">Dalip Tandon</h5>
             <p className="bg-gradient-to-r from-purple-600 to-blue-400 text-transparent bg-clip-text">leetcode wrapped 2024</p>
             <div>
@@ -15,13 +16,15 @@ const UserProfile=()=>{
                 <Content/>
             </div>
             </div>
-            <div className=" h-1/6 mt-5 w-3/6 p-5    flex justify-center">
+            <div className=" h-fit  mt-5 w-3/6 p-2    flex justify-center absolute bottom-10">
             <button className="border border-white text-white p-2 w-40 font-bold text-xl rounded-lg">Download</button>
             <button className="mx-5 border border-white text-white bg-gradient-to-r from-blue-500 to-purple-600 p-2 w-52 font-bold text-md whitespace-nowrap rounded-lg hover:from-blue-600 hover:to-purple-700 transition  ">Generate Your Wrapped</button>
             </div>
-        </div>
-    )
+    </BackgroundLines>
+  );
 }
+
+        
 
 
 export default UserProfile;
